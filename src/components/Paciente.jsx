59d165d46924707b5a179b1f3a82902d47fc588d
soygussil <gussilv1995@gmail.com>
2022-07-28 21:09:17 -0500

    terminé el proyecto

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,112 @@
+import Swal from "sweetalert2";
+const Paciente = ({paciente, setPaciente,eliminarPaciente}) => {
+
+    const {nombre, propietario, email, fecha, sintomas, id}=paciente;
+
+    const handleEliminar=()=>{
+
+
+        const swalWithBootstrapButtons = Swal.mixin({
+            customClass: {
+              confirmButton: 'py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold  uppercase rounded-lg',
+              cancelButton: 'py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold  uppercase rounded-lg'
+            },
+            buttonsStyling: false
+          })
+          
+          swalWithBootstrapButtons.fire({
+            title: '¿Estas seguro?',
+            text: "Deseas eliminar el paciente?",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonText: 'Borrar',
+            cancelButtonText: 'Cancelar',
+            reverseButtons: true
+          }).then((respuesta) => {
+            if (respuesta.isConfirmed) {
+                eliminarPaciente(id)
+              swalWithBootstrapButtons.fire(
+                'Borrado',
+                'El paciente fue eliminado.',
+                'success'
+              )
+            } else if (
+              /* Read more about handling dismissals below */
+              respuesta.dismiss === Swal.DismissReason.cancel
+            ) {
+              swalWithBootstrapButtons.fire(
+                'Cancelado',
+                'El paciente no fue eliminado',
+                'error'
+              )
+            }
+          })
+
+/*
+        Swal.fire({
+            title: '¿Estas seguro?',
+            text: "Deseas eliminar el paciente?",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Borrar'
+        }).then((respuesta) => {
+            if (respuesta.isConfirmed) {
+                eliminarPaciente(id)
+            Swal.fire(
+                '¡Borrado!',
+                'Él paciente ha sido borrado satisfactoriamente..',
+                'success'
+            )
+        }
+        })
+*/
+        /* 
+        const respuesta= confirm('Deseas eliminar este paciente?');
+        if(respuesta){
+            eliminarPaciente(id)
+        }
+        */
+    }
+    return (
+        <div className='mx-5 my-8 bg-white shadow-md px-5 py-10 rounded-xl'>
+            <p className='font-bold mb-3 text-gray-700 uppercase'>
+                Nombre: {''}
+                <span className='font-normal normal-case'>{nombre}</span>
+            </p>
+            <p className='font-bold mb-3 text-gray-700 uppercase'>
+                Propietario: {''}
+                <span className='font-normal normal-case'>{propietario}</span>
+            </p>
+            <p className='font-bold mb-3 text-gray-700 uppercase'>
+                Email: {''}
+                <span className='font-normal normal-case'>{email}</span>
+            </p>
+            <p className='font-bold mb-3 text-gray-700 uppercase'>
+                Fecha alta: {''}
+                <span className='font-normal normal-case'>{fecha}</span>
+            </p>
+            <p className='font-bold mb-3 text-gray-700 uppercase'>
+                Síntomas: {''}
+                <span className='font-normal normal-case'>{sintomas}</span>
+            </p>
+
+            <div className="flex justify-between mt-10">
+                <button
+                type="button"
+                className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+                onClick={()=>setPaciente(paciente)}
+                >Editar</button>
+                <button
+                type="button"
+                className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold  uppercase rounded-lg"
+                onClick={handleEliminar}
+                >Eliminar</button>
+            </div>
+
+        </div>
+    )
+}
+
+export default Paciente
